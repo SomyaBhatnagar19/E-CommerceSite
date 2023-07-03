69182c4438789da584fa56b39a42d8cfb5deacb9
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-07-03 11:09:43 +0530

    Making the product array with hard coded data visible on screen.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import ProductList from "./Components/Product/ProductList";
 const App = () => {
   return (
-    <>
+    <div>
        This is App Component. 
-    </>
+       <ProductList/>
+    </div>
   )
 }
 export default App;
